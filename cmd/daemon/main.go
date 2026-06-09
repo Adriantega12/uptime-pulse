b10b3695807646060cee4efadda47ff42c1b8121
Adriantega12 <tavo_terrazasg@hotmail.com>
2026-06-09 12:37:47 -0600

    HTMX rendering is complete

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 	"uptime-pulse/internal/engine"
+	"uptime-pulse/internal/web"
 
 	_ "modernc.org/sqlite"
 )
@@ -14,9 +16,19 @@ import (
 func main() {
 	// debug.PrintStack()
 
-	// Initialize DB
+	// Initialize engines
 	uptimeEngine := engine.NewUptimeEngine()
+	webServer := web.NewServer(uptimeEngine)
 
+	// Web server logic
+	http.HandleFunc("/", webServer.HandleDashboard)
+	http.HandleFunc("/api/pings", webServer.HandlePingsAPI)
+
+	go func() {
+		http.ListenAndServe(":8080", nil)
+	}()
+
+	// Uptime engine logic
 	// Channel to capture operating system termination interrupts (e.g., Ctrl+C, systemd stop)
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
@@ -30,7 +42,6 @@ func main() {
 			select {
 			case <-sigChan:
 				fmt.Println("Done! Running cleanup...")
-				uptimeEngine.CloseUptimeEngineStatements()
 				done <- true
 				return
 			case t := <-ticker.C:
@@ -41,5 +52,8 @@ func main() {
 	}()
 
 	<-done
+
+	// Cleanup sequence
+	uptimeEngine.CloseUptimeEngineStatements()
 	fmt.Println("Ticker stopped")
 }
